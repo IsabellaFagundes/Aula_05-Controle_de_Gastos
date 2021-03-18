3571b5fcbae7e9fe0def443131f001c3c175f979
Isabella Fagundes Barao <bellabarao@icloud.com>
2021-03-18 20:31:41 -0300

    Confirmar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public counter = 0;
+  public maxCounter = 0;
+
+  public increment(){
+    this.counter++;
+    if (this.counter > this.maxCounter){ 
+      this.maxCounter = this.counter;  //se o contador foi maior que o contador máximmo, contador máximo passa a ser igual o contador
+    } // ou this.maxCounter = Math.max(this.counter, this.maxCounter); -> o maxCounter vai ser o maior numero entre maxCounter e Counter
+  }
+
+  public decrement(){
+    this.counter --;
+    if(this.counter <= 0){
+      this.counter = 0; //se o contador for menor que 0, ele fica igual a 0
+    }// ou this.counter = Math.max(this.counter -1, 0); -> o maxCounter vai ser o maior numero entre e o valor decrementado e 0
+    
+  }
+
+  public reset(){
+    this.counter = 0;
+  }
 
 }
