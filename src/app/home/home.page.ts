615ea9b93c0f7737016a6edbae8a9388b69c3ec0
Isabella Fagundes Barão <bellabarao@icloud.com>
2021-04-27 23:00:55 -0300

    Adicionando Conta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,7 @@ export class HomePage {
 
   public counter = 0;
   public maxCounter = 0;
+  public selectedValue = 0;
 
   public increment(){
     this.counter++;
@@ -26,6 +27,23 @@ export class HomePage {
   }
 
   public reset(){
+    this.counter = 0;
+  }
+
+  public incrementConta(){
+    this.counter+= this.selectedValue;
+    this.selectedValue = 0;
+    if (this.counter > this.maxCounter){ 
+      this.maxCounter = this.counter;  //se o contador foi maior que o contador máximmo, contador máximo passa a ser igual o contador
+    } // ou this.maxCounter = Math.max(this.counter, this.maxCounter); -> o maxCounter vai ser o maior numero entre maxCounter e Counter
+  }
+
+  public decrementConta(){
+    this.counter = Math.max(this.counter-this.selectedValue, 0);
+    this.selectedValue = 0;
+  }
+
+  public resetConta(){
     this.counter = 0;
   }
 
